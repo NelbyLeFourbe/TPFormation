e94de770e6ac61cbec84437d1d7a4e71e06ec651
Unknown <valentin.peclard@hotmail.fr>
2018-08-10 16:03:14 +0200

    update

--- formation-jpa/src/main/java/sopra/promo404/formation/dao/IDAO.java
+++ formation-jpa/src/main/java/sopra/promo404/formation/dao/IDAO.java
@@ -1,5 +1,15 @@
 package sopra.promo404.formation.dao;
 
-public interface IDAO {
+import java.util.List;
 
+public interface IDAO<T, PK> {
+	List<T> findAll();
+
+	T findById(PK id);
+
+	T save(T entity);
+
+	void delete(T entity);
+
+	void deleteById(PK id);
 }
---
+++ formation-jpa/src/main/java/sopra/promo404/formation/dao/IDAOFormateur.java
@@ -0,0 +1,7 @@
+package sopra.promo404.formation.dao;
+
+import sopra.promo404.formation.model.Formateur;
+
+public interface IDAOFormateur extends IDAO<Formateur, Long> {
+
+}
